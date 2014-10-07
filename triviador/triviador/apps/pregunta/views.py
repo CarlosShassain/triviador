becd8b980418db6f96aa5812d3c06a7525c8b4f8
Carlos <shassain_666@hotmail.com>
2014-10-07 17:14:43 -0600

    quinta

--- triviador/triviador/apps/pregunta/views.py
+++ triviador/triviador/apps/pregunta/views.py
@@ -1,29 +1,45 @@
 #encoding:utf-8
-from django.shortcuts import render, render_to_response
-from django.http import HttpResponse
-from django.template import RequestContext
 
+from .forms import *
+from .models import *
 from django.contrib.auth.models import User
 
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login,logout,authenticate
 from django.http import HttpResponseRedirect
-#from django.contrib.auth import RequestContext
+
+from django.shortcuts import render_to_response, redirect, render
+from django.template import RequestContext, loader, Context, Template
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse 
+from django.http import *
 
 from .models import PerfilUser
 def pagina_index(request):
 	return render_to_response("blog/index.html",{},context_instance=RequestContext(request))
+#def registro(request):
+#	if request.method=="POST":
+#		form=PerfilUser_Form(request.POST)
+#		if(form.is_valid()):
+#			form.save()
+#			return HttpResponseRedirect("/blog/")
+#	form=PerfilUser_Form()
+#	return render_to_response("usuario/registro.html",{"form":form},RequestContext(request))
 def registro(request):
-	if request.method=="POST":
-		fusuario=UserCreationForm(request.POST)
-		if(fusuario.is_valid()):
-			fusuario.save();
-			usuario=request.POST["username"]
-			pais=request.POST["pais"]
-			telefono=request.POST["telefono"]
-			nuevo=User.objects.get(username=usuario)
-			perfil=PerfilUser.objects.create(user=nuevo,pais=pais,telefono=telefono)
-			return HttpResponse("Registrado")
+	username = password = email =''
+	if request.POST:
+		user_form = UserCreateForm(request.POST)
+		if user_form.is_valid():
+			usuario = User(username=request.POST['username'], email=request.POST['email'])
+			usuario.set_password(request.POST['password1'])
+			usuario.save()
+			return HttpResponseRedirect("/blog/")
 	else:
-		fusuario=UserCreationForm()
-	return render_to_response("usuario/registro.html",{'formulario':fusuario},context_instance=RequestContext(request))
+		user_form = UserCreateForm()
+
+	diccionario = {
+		'user_form': user_form,
+		'page_title': 'Aplicacion - Register',
+		'body_class': 'register',
+	}
+	return render_to_response("usuario/registro.html", diccionario, context_instance=RequestContext(request))
